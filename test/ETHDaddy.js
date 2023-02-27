02b5b14cfa1b9bb221e889a860cba63a97c0674e
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-02-28 02:01:29 +0600

    web3 domain site

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -6,35 +6,70 @@ const tokens = (n) => {
 
 describe("ETHDaddy", () => {
   let ethDaddy
-  let deployer,owner1
+  let deployer, owner1
 
-  const NAME = 'ETH Daddy'
-  const SYMBOL = 'ETHD'
+  const NAME = "ETH Daddy"
+  const SYMBOL = "ETHD"
 
-  beforeEach(async() =>{
-   
-    [deployer,owner1] = await ethers.getSigners()
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, owner1] = await ethers.getSigners()
 
-    const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
-    ethDaddy = await ETHDaddy.deploy('ETH Daddy','ETHD')
+    // Deploy contract
+    const ETHDaddy = await ethers.getContractFactory("ETHDaddy")
+    ethDaddy = await ETHDaddy.deploy(NAME, SYMBOL)
+
+    // List a domain
+    const transaction = await ethDaddy.connect(deployer).list("jack.eth", tokens(10))
+    await transaction.wait()
   })
 
-  describe('Deployment',() => {
-    it('has a name', async () => {
+  describe("Deployment", () => {
+
+    it("Sets the name", async () => {
       const result = await ethDaddy.name()
       expect(result).to.equal(NAME)
     })
-  
-    it('has a symbol', async () => {
+
+    it("Sets the symbol", async () => {
       const result = await ethDaddy.symbol()
       expect(result).to.equal(SYMBOL)
     })
 
-    it("Sets the owner", async() => {
-      const result = await ethDaddy.owner();
+    it("Sets the owner", async () => {
+      const result = await ethDaddy.owner()
       expect(result).to.equal(deployer.address)
     })
 
+    it("Returns the max supply", async () => {
+      const result = await ethDaddy.maxSupply()
+      expect(result).to.equal(1)
+    })
+
+})
+
+describe("Domain", () => {
+  it('Returns domain attributes', async () => {
+    let domain = await ethDaddy.getDomain(1);
+    expect(domain.name).to.be.equal("jack.eth")
+    expect(domain.cost).to.be.equal(tokens(10))
+    expect(domain.isOwned).to.be.equal(false)
   })
+})
+
+describe("Minting",() => {
+  const ID = 1
+    const AMOUNT = ethers.utils.parseUnits("10", 'ether')
+
+  beforeEach(async () => {
+    const transaction = await ethDaddy.connect(owner1).mint(ID)
+    await transaction.wait()
+  })
+
+  it('Updates the owner', async () => {
+    const owner = await ethDaddy.ownerOf(ID)
+    expect(owner).to.be.equal(owner1.address)
+  })
+})
 
 })
